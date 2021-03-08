8567dfff56963cfc1893386b740bb2a943c85d9b
trent <trent.hilliam@vorteil.io>
2021-03-08 13:14:19 +1000

    Fixed azure cli

--- azcli/main.go
+++ azcli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/vorteil/direktiv-apps/pkg/direktivapps"
@@ -26,19 +27,19 @@ func main() {
 	direktivapps.ReadIn(obj, g)
 
 	// Authenticate with the azcli using a service principal
-	cmd := exec.Command("/usr/bin/az", "--service-principal", "login", "-u", obj.Name, "-p", obj.Password, "--tenant", obj.Tenant)
-	err = cmd.Run()
+	cmd := exec.Command("/usr/bin/az", "login", "--service-principal", "-u", obj.Name, "-p", obj.Password, "--tenant", obj.Tenant)
+	resp, err := cmd.CombinedOutput()
 	if err != nil {
-		g.ErrorMessage = err.Error()
+		g.ErrorMessage = fmt.Sprintf("output: %s", resp)
 		direktivapps.WriteError(g)
 	}
 
 	// Execute command provided via the input of container
 	cmd = exec.Command("/usr/bin/az", obj.Command...)
 
-	resp, err := cmd.CombinedOutput()
+	resp, err = cmd.CombinedOutput()
 	if err != nil {
-		g.ErrorMessage = err.Error()
+		g.ErrorMessage = fmt.Sprintf("output: %s", resp)
 		direktivapps.WriteError(g)
 	}
 
