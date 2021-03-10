da15a3b881536015ca6ae7d42961e850f50de5e7
Jon Alfaro <jon.alfaro@vorteil.io>
2021-03-11 09:04:22 +1000

    google cli added
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

---
+++ googlecli/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+
+	"github.com/vorteil/direktiv-apps/pkg/direktivapps"
+)
+
+// InputContainerDetails ...
+type InputContainerDetails struct {
+	ServiceAccountKey string   `json:"serviceAccountKey"`
+	Command           []string `json:"command"`
+}
+
+func main() {
+
+	g := direktivapps.ActionError{
+		ErrorCode:    "com.googlecli.error",
+		ErrorMessage: "",
+	}
+
+	obj := new(InputContainerDetails)
+	direktivapps.ReadIn(obj, g)
+
+	err := ioutil.WriteFile("/key.json", []byte(obj.ServiceAccountKey), 0644)
+	if err != nil {
+		g.ErrorMessage = fmt.Sprintf("could not write key: %s", err)
+		direktivapps.WriteError(g)
+	}
+
+	cmd := exec.Command("/usr/bin/gcloud", "auth", "activate-service-account", "/key.json")
+	resp, err := cmd.CombinedOutput()
+	if err != nil {
+		g.ErrorMessage = fmt.Sprintf("failed auth: %s", resp)
+		direktivapps.WriteError(g)
+	}
+
+	cmd = exec.Command("/usr/bin/gcloud", obj.Command...)
+	resp, err = cmd.CombinedOutput()
+	if err != nil {
+		g.ErrorMessage = fmt.Sprintf("%s", resp)
+		direktivapps.WriteError(g)
+	}
+
+	direktivapps.WriteOut(resp, g)
+
+}
