3eae2afdb5d528ce34b7b4294cfa90c6def85f2c
Jon Alfaro <jon.alfaro@vorteil.io>
2021-04-08 09:04:12 +1000

    aws-ec2-create: new validator added
    Signed-off-by: Jon Alfaro <jon.alfaro@vorteil.io>

--- aws-ec2-create/go.mod
+++ aws-ec2-create/go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/vorteil/direktiv-apps v0.0.0-20210407040033-8b5bd4028c2e // indirect
+	github.com/vorteil/direktiv-apps v0.0.0-20210407225944-09443177654f
 )
--- aws-ec2-create/main.go
+++ aws-ec2-create/main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-playground/validator"
 	"log"
 	"os"
 	"os/exec"
-	"reflect"
 	"text/template"
 
 	"bytes"
 	"github.com/vorteil/direktiv-apps/pkg/direktivapps"
+	"github.com/vorteil/direktiv-apps/pkg/validator"
 	"strings"
 )
 
@@ -56,28 +55,15 @@ func main() {
 	obj := new(InputInstanceDetails)
 	direktivapps.ReadIn(obj, g)
 
-	// Create Validator
-	v := validator.New()
-	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
-		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
-
-		if name == "-" {
-			return ""
-		}
-
-		return name
-	})
-
 	// Validate Input
-	errArr := v.Struct(obj)
-	if errArr != nil {
-		invalidFields := make([]string, 0)
-		for _, e := range errArr.(validator.ValidationErrors) {
-			log.Printf("Input Error: %s is %s\n", e.Field(), e.Tag())
-			invalidFields = append(invalidFields, e.Field())
+	v := validator.CreateValidator()
+
+	if missingFields := v.ValidateRequired(obj); len(missingFields) > 0 {
+		for _, mf := range missingFields {
+			log.Printf("Input Error: %s is required\n", mf)
 		}
 
-		g.ErrorMessage = fmt.Sprintf("Invalid input: Fields [%s] are required", strings.Join(invalidFields, ","))
+		g.ErrorMessage = fmt.Sprintf("Invalid input: Fields [%s] are required", strings.Join(missingFields, ","))
 		direktivapps.WriteError(g)
 	}
 
