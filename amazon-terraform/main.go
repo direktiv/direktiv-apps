21b96f23bfa8d2ff99ed903598ab1fcf2929cfbe
trent <trent.hilliam@vorteil.io>
2021-05-25 15:07:18 +1000

    Added terraform containers that return the tfstate of an action.

---
+++ amazon-terraform/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+
+	"github.com/vorteil/direktiv-apps/pkg/direktivapps"
+)
+
+type TerraformInput struct {
+	Tf        string                 `json:"url"`        // url that wgets the terraform file
+	Tfstate   map[string]interface{} `json:"tfstate"`    // the tfstate being parsed into
+	Action    string                 `json:"action"`     // "plan", "validate", "apply", "destroy"
+	AccessKey string                 `json:"access_key"` // accesskey
+	SecretKey string                 `json:"secret_key"` // secret key
+}
+
+var code = "com.terraform.error"
+var terraformBin = "/terraform"
+
+type CMDWriter struct {
+	Aid string
+}
+
+func (c *CMDWriter) Write(p []byte) (n int, err error) {
+	direktivapps.Log(c.Aid, string(p))
+	return len(p), nil
+}
+
+func main() {
+	direktivapps.StartServer(TerraformHandler)
+}
+
+func TerraformHandler(w http.ResponseWriter, r *http.Request) {
+
+	obj := new(TerraformInput)
+	aid, err := direktivapps.Unmarshal(obj, r)
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+
+	// set tf state
+	if obj.Tfstate != nil {
+		tfstate, err := json.Marshal(obj.Tfstate)
+		if err != nil {
+			direktivapps.RespondWithError(w, code, err.Error())
+			return
+		}
+		direktivapps.Log(aid, fmt.Sprintf("\n\n\n%+v\n\n\n", tfstate))
+
+		err = ioutil.WriteFile("./terraform.tfstate", tfstate, os.ModePerm)
+		if err != nil {
+			direktivapps.RespondWithError(w, code, err.Error())
+			return
+		}
+	}
+
+	os.Setenv("TF_LOG", "DEBUG")
+
+	// setup custom writer to send command output to direktiv
+	cmdW := &CMDWriter{
+		Aid: aid,
+	}
+
+	direktivapps.Log(aid, "Fetching TF file externally")
+
+	resp, err := http.Get(obj.Tf)
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+
+	direktivapps.Log(aid, "Generating authentication for terraform...")
+
+	// set aws auth
+
+	err = os.Setenv("AWS_ACCESS_KEY_ID", obj.AccessKey)
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+
+	err = os.Setenv("AWS_SECRET_ACCESS_KEY", obj.SecretKey)
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+
+	direktivapps.Log(aid, "Creating the tf file for terraform to read from...")
+	// write terraform file from string
+	err = ioutil.WriteFile("/main.tf", []byte(data), os.ModePerm)
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+
+	direktivapps.Log(aid, "Initializing terraform...")
+	// initialize the terraform get required plugins etc
+	init := exec.Command(terraformBin, "init")
+	init.Stderr = cmdW
+	init.Stdout = cmdW
+	err = init.Run()
+	if err != nil {
+		direktivapps.RespondWithError(w, code, err.Error())
+		return
+	}
+
+	direktivapps.Log(aid, "Executing action for terraform...")
+	switch obj.Action {
+	case "apply":
+		fallthrough
+	case "destroy":
+		cmd := exec.Command(terraformBin, obj.Action, "-auto-approve")
+		cmd.Stdout = cmdW
+		cmd.Stderr = cmdW
+		err = cmd.Run()
+		if err != nil {
+			direktivapps.RespondWithError(w, code, err.Error())
+			return
+		}
+	case "validate":
+		fallthrough
+	case "plan":
+		cmd := exec.Command(terraformBin, obj.Action)
+		cmd.Stdout = cmdW
+		cmd.Stderr = cmdW
+		cmd.Run()
+		if err != nil {
+			direktivapps.RespondWithError(w, code, err.Error())
+			return
+		}
+	}
+
+	state, err := ioutil.ReadFile("terraform.tfstate")
+	if err != nil {
+		if os.IsNotExist(err) {
+		} else {
+			direktivapps.RespondWithError(w, code, err.Error())
+			return
+		}
+	}
+
+	if state == nil {
+		direktivapps.Respond(w, []byte("{}"))
+	}
+
+	direktivapps.Respond(w, state)
+}
---
+++ google-terraform/go.mod
@@ -0,0 +1,5 @@
+module github.com/vorteil/direktiv-apps/terraform
+
+go 1.16
+
+require github.com/vorteil/direktiv-apps v0.0.0-20210511035918-ae478c1e4743 // indirect
