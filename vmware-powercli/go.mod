e0c56e243e2de1a2d0074bcd0c4dff58f0144a15
Jens Gerke <jens.gerke@direktiv.io>
2022-03-10 20:37:23 +1000

    update powershell
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- vmware-powercli/go.mod
+++ vmware-powercli/go.mod
@@ -3,8 +3,8 @@ module github.com/direktiv/direktiv-apps/vmware-powercli
 go 1.16
 
 require (
-	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
-	github.com/direktiv/direktiv-apps/pkg v0.0.0-20220210025353-e6a0dfca5e90
-	github.com/direktiv/go-powershell v0.0.0-20220218012621-5a23fa8df6bb
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d
+	github.com/direktiv/direktiv-apps/pkg v0.0.0-20220308235314-03b03dbf3802
+	github.com/direktiv/go-powershell v0.0.0-20220309000203-2dd9979acc44
+	github.com/google/uuid v1.3.0
 )
--- vmware-powercli/main.go
+++ vmware-powercli/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/acarl005/stripansi"
 	"github.com/direktiv/direktiv-apps/pkg/reusable"
@@ -24,15 +26,128 @@ type CmdOutput struct {
 	Success bool        `json:"result"`
 	Stdout  interface{} `json:"stdout"`
 	Stderr  interface{} `json:"stderr"`
-	Output  interface{} `json:"output"`
+	Output  interface{} `json:"output,omitempty"`
 }
 
 type requestInput struct {
 	Scripts  []Script `json:"scripts"`
 	Continue bool     `json:"continue"`
-	Host     string   `json:"host"`
-	User     string   `json:"user"`
-	Pwd      string   `json:"password"`
+
+	ri *reusable.RequestInfo `json:"-"`
+
+	errCh    chan error                 `json:"-"`
+	resultCh chan map[string]*CmdOutput `json:"-"`
+	shell    pss.Shell                  `json:"-"`
+}
+
+const shells = 5
+
+var jobs chan *requestInput
+
+func runScripts(obj *requestInput, shell pss.Shell, be *backend.Local) (map[string]*CmdOutput, error) {
+
+	be.Writer = obj.ri.LogWriter()
+
+	shell.Execute(fmt.Sprintf("Set-Location -Path %s", obj.ri.Dir()))
+
+	obj.ri.Logger().Infof("getting shell")
+
+	object := make(map[string]*CmdOutput)
+
+	for i := range obj.Scripts {
+
+		co := &CmdOutput{
+			Success: true,
+		}
+		stdout, stderr, err := executeScript(obj.ri, shell, obj.Scripts[i])
+
+		if err != nil {
+			obj.ri.Logger().Errorf("error executing script: %v", err)
+
+			// stop if one script fails
+			if !obj.Continue {
+				return nil, err
+			}
+			co.Success = false
+		}
+
+		// add to result
+		co.Stderr = stderr
+		co.Stdout = toJSON(obj.ri, stdout)
+
+		// adding output file if configured
+		if co.Success && len(obj.Scripts[i].Output) > 0 {
+
+			obj.ri.Logger().Infof("getting output file")
+
+			uf := fmt.Sprintf("%s/%s", obj.ri.Dir(), obj.Scripts[i].Output)
+			defer os.Remove(uf)
+
+			o, err := os.ReadFile(uf)
+			if err != nil {
+				if !obj.Continue {
+					return nil, err
+				}
+				co.Success = false
+			}
+
+			co.Output = toJSON(obj.ri, string(o))
+		}
+
+		object[obj.Scripts[i].Script.Name] = co
+	}
+
+	return object, nil
+}
+
+func runPwsh(input chan *requestInput) {
+
+	log.Println("creating shell")
+
+	var err error
+	be, shell, err := getShell()
+	if err != nil {
+		log.Printf("can not create shell: %v", err)
+	}
+
+	for {
+
+		log.Println("worker waiting for request")
+
+		obj := <-input
+
+		// we add this so we can timeout in the script later
+		obj.shell = shell
+
+		log.Println("worker got request")
+
+		// get a new shell if required
+		if shell == nil {
+			log.Println("shell nil, creating new one")
+			be, shell, err = getShell()
+			if err != nil {
+				obj.ri.Logger().Errorf("can not create shell: %v", err)
+				continue
+			}
+		}
+
+		result, err := runScripts(obj, shell, be)
+		if err != nil {
+			obj.ri.Logger().Errorf("can not execute shell: %v", err)
+			obj.errCh <- err
+		} else {
+			obj.resultCh <- result
+		}
+
+		// trying to get a new shell
+		shell.Exit()
+		be, shell, err = getShell()
+		if err != nil {
+			log.Printf("can not create shell: %v", err)
+		}
+
+	}
+
 }
 
 func powercliHandler(w http.ResponseWriter, r *http.Request, ri *reusable.RequestInfo) {
@@ -44,70 +159,27 @@ func powercliHandler(w http.ResponseWriter, r *http.Request, ri *reusable.Reques
 		return
 	}
 
-	// create the shell
-	sh, err := getShell(ri, obj.Host, obj.User, obj.Pwd)
-	if err != nil {
-		reusable.ReportError(w, errForCode("shell"), err)
-		return
+	obj.ri = ri
+	obj.errCh = make(chan error, 1)
+	obj.resultCh = make(chan map[string]*CmdOutput, 1)
+
+	jobs <- obj
+
+	select {
+	case err = <-obj.errCh:
+		reusable.ReportError(w, "com.powershell.run.error", err)
+	case result := <-obj.resultCh:
+		reusable.ReportResult(w, result)
+	case <-time.After(60 * time.Minute):
+		reusable.ReportError(w, "com.powershell.timeout.error", fmt.Errorf("timed out"))
 	}
-	defer sh.Exit()
-
-	object := make(map[string]*CmdOutput)
-
-	for i := range obj.Scripts {
-		co := &CmdOutput{
-			Success: true,
-		}
-		stdout, stderr, err := executeScript(ri, sh, obj.Scripts[i])
-
-		if err != nil {
-			ri.Logger().Errorf("error executing script: %v", err)
-
-			// stop if one script fails
-			if !obj.Continue {
-				reusable.ReportError(w, reusable.UnmarshallError, err)
-				return
-			}
-			co.Success = false
-		}
-
-		// add to result
-		co.Stderr = stderr
-		co.Stdout = toJSON(ri, stdout)
-
-		if co.Success && len(obj.Scripts[i].Output) > 0 {
-			ri.Logger().Infof("getting output file")
-
-			uf := fmt.Sprintf("%s/%s", ri.Dir(), obj.Scripts[i].Output)
-
-			b, err := os.ReadFile(uf)
-			if err != nil {
-				reusable.ReportError(w, errForCode("output"), err)
-				return
-			}
-
-			ret := make(map[string]interface{})
-			err = json.Unmarshal(b, &ret)
-			if err != nil {
-				reusable.ReportError(w, errForCode("json"), err)
-				return
-			}
-			defer os.Remove(uf)
-			co.Output = ret
-		}
-
-		object[obj.Scripts[i].Script.Name] = co
-
-	}
-
-	reusable.ReportResult(w, object)
 
 }
 
 func executeScript(ri *reusable.RequestInfo, shell pss.Shell, script Script) (string, string, error) {
 
 	// create file
-	f, err := script.Script.AsFile(0755)
+	f, err := script.Script.AsFile(ri, 0755)
 	if err != nil {
 		ri.Logger().Errorf("error creating script: %v", err)
 		return "", "", err
@@ -117,9 +189,7 @@ func executeScript(ri *reusable.RequestInfo, shell pss.Shell, script Script) (st
 	if name == "" {
 		name = uuid.New().String()
 	}
-
 	ps1 := fmt.Sprintf("%s.ps1", name)
-
 	os.Rename(name, ps1)
 	defer os.Remove(ps1)
 
@@ -131,59 +201,28 @@ func executeScript(ri *reusable.RequestInfo, shell pss.Shell, script Script) (st
 		ri.Logger().Errorf("error executing script: %v", err)
 		return stdout, stderr, err
 	}
-	printOut(ri, stdout, stderr)
 
 	return stdout, stderr, err
 }
 
-func getShell(ri *reusable.RequestInfo, server, user, password string) (pss.Shell, error) {
+func getShell() (*backend.Local, pss.Shell, error) {
 
-	ri.Logger().Infof("creating shell")
+	back := &backend.Local{}
 
-	back := &backend.Local{
-		Dir: ri.Dir(),
-	}
-	shell, err := pss.New(back)
-	if err != nil {
-		ri.Logger().Errorf("error creating new shell: %v", err)
-		return nil, err
-	}
+	s, err := pss.New(back)
 
-	if server != "" {
-		ri.Logger().Infof("connecting to %s", server)
-		o, e, err := shell.Execute(fmt.Sprintf("Connect-VIServer -Server %s -User %s -Password %s", server, user, password))
-		if err != nil {
-			ri.Logger().Errorf("error connecting to %s: %v", server, err)
-			return nil, err
-		}
-		printOut(ri, o, e)
-	}
-
-	_, _, err = shell.Execute("$WarningPreference = 'SilentlyContinue'")
-	if err != nil {
-		ri.Logger().Errorf("error executing SilentlyContinue: %v", err)
-		return nil, err
-	}
-
-	return shell, nil
+	return back, s, err
 
 }
 
 func main() {
+
+	jobs = make(chan *requestInput, shells)
+	for a := 0; a < shells; a++ {
+		go runPwsh(jobs)
+	}
+
 	reusable.StartServer(powercliHandler, nil)
-}
-
-func errForCode(errCode string) string {
-	return fmt.Sprintf("com.powershell.%s.error", errCode)
-}
-
-func printOut(ri *reusable.RequestInfo, o, e string) {
-	if len(o) > 0 {
-		ri.Logger().Infof(fmt.Sprintf("shell stdout: %v", o))
-	}
-	if len(e) > 0 {
-		ri.Logger().Infof(fmt.Sprintf("shell stderr: %v", e))
-	}
 }
 
 func toJSON(ri *reusable.RequestInfo, str string) interface{} {
@@ -194,10 +233,6 @@ func toJSON(ri *reusable.RequestInfo, str string) interface{} {
 	var js json.RawMessage
 	err := json.Unmarshal([]byte(str), &js)
 	if err != nil {
-
-		if e, ok := err.(*json.SyntaxError); ok {
-			ri.Logger().Infof("syntax error at byte offset %d", e.Offset)
-		}
 		return str
 	}
 
